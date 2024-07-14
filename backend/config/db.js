bf719fdf64e51b6c3b5e2185fb2ec0bba4fd315a
dheeraj404 <2021itb082.dheeraj@students.iiests.ac.in>
2024-07-14 11:40:09 +0530

    changing some functions

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 const connectToDb = () => {
   mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.DB_URI)
     .then((data) =>
       console.log(`Database connected with ${data.connection.host}`)
     )
